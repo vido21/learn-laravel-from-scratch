426d795a6d8fb3b1783385c59b2e5771217bc16a
vido21 <vidobae@gmail.com>
2020-05-16 00:42:16 +0700

    set up tweety Desain

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,23 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+            <form action="">
+
+                <textarea 
+                    name="body" 
+                    class="w-full"
+                    placeholder="What do you think!!"
+                ></textarea>
+                
+                <hr class="my-4">
+
+                <footer class="flex justify-between">
+                    <img 
+                        src="https://picsum.photos/60" 
+                        alt="Avatar"
+                        class="rounded-full mr-2"
+                    >
+
+                    <button type="submit" class="bg-blue-500 rounded-lg shadow py-4 px-2 text-white">Tweet a root</button>
+                </footer>
+
+            </form>
+        </div>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,18 @@
+<div class="flex p-4 border-b border-b-gray-400"> 
+
+<div class="mr-2 flex-shrink-0">
+    <img 
+        src="https://picsum.photos/50" 
+        alt="Avatar"
+        class="rounded-full mr-2"
+    >
+</div>
+
+<div>
+    <h5 class="font-bold mb-4">John Doe</h5>
+    <p class="text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, maiores cum. Iure, consectetur perspiciatis! Nostrum eum inventore ullam modi corrupti illum accusamus qui laboriosam placeat voluptatibus? Hic voluptatibus eius mollitia.
+
+    </p>
+</div>
+</div>
